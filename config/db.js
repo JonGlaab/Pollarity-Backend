1f3010eb2ae5f5044fb93dc64714cc11261d9050
ElizabethMokrusa <elimokrusa@msn.com>
2025-11-19 07:11:55 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -34,7 +34,7 @@ if (process.env.DB_SSL === 'true' || process.env.DB_SSL === 'required') {
 const sequelize = new Sequelize(
     process.env.DB_NAME,
     process.env.DB_USER,
-    process.env.DB_PASS,
+    process.env.DB_PASSWORD,
     {
         host: process.env.DB_HOST,
         port: process.env.DB_PORT || 3306,
