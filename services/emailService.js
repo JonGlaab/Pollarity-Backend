94d32252d2b058c1a3edd3723d78a3291b612be5
LouisV-MT <traceur.vu96@gmail.com>
2025-11-21 09:44:41 -0500

    Update sender email address in welcome email
    Changed the 'from' address in the sendWelcomeEmail function from 'onboarding@yourdomain.com' to 'Pollarity@proton.me' to reflect the correct sender identity.

--- services/emailService.js
+++ services/emailService.js
@@ -5,7 +5,7 @@ const sendWelcomeEmail = async (to, firstName) => {
     try {
         await resend.emails.
         send({
-            from: 'onboarding@yourdomain.com',
+            from: 'Pollarity@proton.me',
             to: to,
             subject: `Welcome to Pollarity, ${firstName}!`,
             html: `<h1>Hi ${firstName},</h1><p>Thanks for signing up for Pollarity. We're excited to have you on board!</p>`
